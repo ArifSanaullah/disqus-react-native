2a097d306352a1cdc08c226af9488e5e3bf76fb3
Arif Sanaullah <ak7896023@gmail.com>
2025-06-11 17:22:41 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div style={{ padding: 20 }}>
+            <h1>Welcome to My Vite + Disqus App</h1>
+            <p>This React app includes Disqus comments below.</p>
+        </div>
+    );
+}
+
+export default App;
